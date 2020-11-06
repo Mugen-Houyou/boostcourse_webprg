4f7f9f53ce7802e51c766661a1239472fd15b9a9
aaaaa <a4r2@daum.net>
2020-11-06 22:45:44 +0900

    1주차 최초 업로드

---
+++ be-pjt-a-1-aboutme/src/example/TodayServlet.java
@@ -0,0 +1,54 @@
+package example;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class timeservlet
+ */
+@WebServlet("/timeservlet")
+public class TodayServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public TodayServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+		response.setContentType("text/html;charset=UTF-8");	
+		
+
+		PrintWriter out = response.getWriter();
+		out.println("<a href=\"./index.html\">메인으로</a>");
+		out.println("</br>");
+		out.println("<h1 style=\"text-align:center;\">");
+		response.getWriter().append("현재시각: ");
+		
+		LocalDateTime now = LocalDateTime.now();
+		DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy년 M월 d일 a h시 m분");
+		String nowString = now.format(dateTimeFormatter);
+		
+		response.getWriter().append(nowString);
+		out.println("</h1>");
+
+		
+	}
+
+}
